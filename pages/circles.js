a3fd589f86a0952b2fb98c299a20c5c66d2adf32
Sharun E Rajeev <sharunpublic@gmail.com>
2021-10-21 12:30:07 +0530

    Templated ted circle page

--- pages/circles.js
+++ pages/circles.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Carousel from '../components/carousel'
 
 const Circles = () => {
     return (
@@ -9,9 +10,30 @@ const Circles = () => {
                 {/* <link rel="icon" href="/favicon.ico" /> */}
             </Head>
 
-            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 mt-52 mb-32">
-                <h1 className="font-bold text-5xl">Circles</h1>
-                <h3 className="text-2xl text-center">A TEDx event is only as phenomenal as the team behind it. Behold, a troupe of carefully selected, brilliant minds, who encompass the essence of TEDx along with a stroke of creativity and enthusiasm. Our 32 volunteers are the hands behind the different parts of every event and together make up the 7 teams of TEDxStTeresasCollege. We proudly stand as one - united by a purpose to spread ideas worth sharing.</h3>
+            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 min-h-full mt-52 mb-32">
+                <h1 className="font-bold text-5xl">Circles Cards</h1>
+            </div>
+
+            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 min-h-full mt-52 mb-32">
+                <h2 className="text-2xl font-semibold">Learn more about TED Circles</h2>
+                <div className="w-96 h-52 bg-gray-800"></div>
+            </div>
+
+            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 min-h-full mt-52 mb-32">
+                <h2 className="text-2xl font-semibold">Camera Roll</h2>
+                <Carousel />
+            </div>
+
+            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 min-h-full mt-52 mb-32">
+                <h2 className="text-2xl font-semibold">Our Next Event</h2>
+                <div className="w-96 h-52 bg-gray-800"></div>
+                <h2 className="text-lg">Date : <span></span></h2>
+                <h2 className="text-lg">Time : <span></span></h2>
+                <h2 className="text-lg">Venue : <span></span></h2>
+                <h2 className="text-lg">Description<span></span></h2>
+                <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                    I'm there! Sign me up!
+                </button>
             </div>
         </>
     );
