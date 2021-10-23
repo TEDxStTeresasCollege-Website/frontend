f03c59b93aa79ad8e652862c9f82511c6021b88c
Sharun E Rajeev <sharunpublic@gmail.com>
2021-10-23 11:38:15 +0530

    config new colours in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 // tailwind.config.js
 module.exports = {
+  mode: "jit",
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -9,6 +10,15 @@ module.exports = {
         '-10': '-10',
         '-20': '-20',
         '-30': '-30',
+      },
+      colors: {
+        'card-1': '#877fa8',
+        'card-2': '#bd86a6',
+        'card-3': '#d47e97',
+        'card-4': '#f59193',
+        'card-5': '#faad91',
+        'card-6': '',
+        'card-7': '',
       }
     },
   },
