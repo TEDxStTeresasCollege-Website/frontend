aae44d25af93a3191dc21ae6318783cae618d3bb
Sharun E Rajeev <sharunpublic@gmail.com>
2021-11-06 22:26:57 +0530

    mobile team

--- components/CategoryButton.js
+++ components/CategoryButton.js
@@ -1,6 +1,6 @@
 const Button = ({text}) => {
     return (
-        <button className="text-black bg-transparent border border-solid border-black hover:bg-black hover:text-white active:bg-purple-600 font-bold uppercase text-sm px-6 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">{text}</button>
+        <button className="text-black bg-transparent border border-solid border-black hover:bg-black hover:text-white active:bg-purple-600 font-bold uppercase text-xs md:text-sm px-6 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">{text}</button>
     );
 }
 
--- pages/team.js
+++ pages/team.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
-import Button from '../components/CategoryButton';
+import Button from '../components/CategoryButton'
 import TeamCard from '../components/TeamCard'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 const Team = () => {
 
@@ -12,11 +14,13 @@ const Team = () => {
                 <link rel="icon" href="icons/logo.ico" />
             </Head>
 
-            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 mt-52 mb-20">
-                <h1 className="font-bold text-5xl">Team</h1>
-                <h3 className="text-2xl text-center">A TEDx event is only as phenomenal as the team behind it. Behold, a troupe of carefully selected, brilliant minds, who encompass the essence of TEDx along with a stroke of creativity and enthusiasm. Our 32 volunteers are the hands behind the different parts of every event and together make up the 7 teams of TEDxStTeresasCollege. We proudly stand as one - united by a purpose to spread ideas worth sharing.</h3>
+            <Navbar />
+
+            <div className="flex flex-col gap-5 items-center text-center justify-center font-sans text-black mx-auto w-3/4 mt-52">
+                <h1 className="font-bold text-2xl md:text-5xl">Team</h1>
+                <h3 className="text-sm md:text-2xl text-center">A TEDx event is only as phenomenal as the team behind it. Behold, a troupe of carefully selected, brilliant minds, who encompass the essence of TEDx along with a stroke of creativity and enthusiasm. Our 32 volunteers are the hands behind the different parts of every event and together make up the 7 teams of TEDxStTeresasCollege. We proudly stand as one - united by a purpose to spread ideas worth sharing.</h3>
             </div>
-            <div className="flex justify-center m-5 gap-3 flex-wrap lg:w-3/4 mx-auto">
+            <div className="flex justify-center m-5 mt-10 gap-3 flex-wrap lg:w-3/4 mx-auto">
                 <Button text="All" />
                 <Button text="Core Team" />
                 <Button text="Content Team" />
@@ -32,6 +36,7 @@ const Team = () => {
                 <TeamCard name="One" designation="Head" social="In" />
             </div>
 
+            <Footer />
         </>
     );
 }
