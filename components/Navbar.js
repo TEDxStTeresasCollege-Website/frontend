e67b4ab2add34643d95a4800665197bde74e2733
Sharun E Rajeev <sharunpublic@gmail.com>
2021-11-06 10:29:51 +0530

    Navbar fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-const Navbar = () => {
+const Navbar = ({ hide }) => {
+
     return (
-        <>
-            <nav className="absolute top-0 flex items-center w-full z-10">
-                <div className="absolute -z-10 w-full h-56">
-                    <Image className="relative object-cover" src="/navbar/navbarbgmobile.svg" layout='fill' />
-                </div>
+        <div className="relative top-0 h-40">
+            <div className={`relative -z-10 w-full h-40 ${hide}`}>
+                <Image className="absolute block object-cover" src="/navbar/navbarbgmobile.svg" layout='fill' />
+            </div>
+            <nav className="absolute top-0 flex items-center w-full z-10 text-sm">
                 <div className="flex-1 px-2 ml-2 md:ml-5">
                     <Link href="/"><Image src="/navbar/tedXSTClogo.svg" width="250%" height="100%" /></Link>
                 </div>
@@ -17,20 +19,24 @@ const Navbar = () => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </button>
-                    <ul className="dropdown-menu absolute hidden text-gray-100 pt-1 right-1 md:right-5 text-right">
-                        <Link href="/"><a className="rounded-t bg-black hover:bg-gray-800 py-4 px-8 block">Home</a></Link>
-                        <Link href="/about"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">About TED/TEDx</a></Link>
-                        <Link href="/about"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">TEDxStTeresasCollege</a></Link>
-                        <Link href="/circles"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">TED Circles</a></Link>
-                        <Link href="/partners"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">Our Partners</a></Link>
-                        <Link href="/blogs"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">Blog</a></Link>
-                        <Link href="/speaker"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">Nominate a Speaker</a></Link>
-                        <Link href="/team"><a className="rounded-b bg-black hover:bg-gray-800 py-4 px-8 block">Meet the Team</a></Link>
+                    <ul className="link-container dropdown-menu hidden">
+                        <Link href="/"><a className="rounded-t navbar-link">Home</a></Link>
+                        <Link href="/about"><a className="navbar-link">About TED/TEDx</a></Link>
+                        <Link href="/about"><a className="navbar-link">TEDxStTeresasCollege</a></Link>
+                        <Link href="/circles"><a className="navbar-link">TED Circles</a></Link>
+                        <Link href="/partners"><a className="navbar-link">Our Partners</a></Link>
+                        <Link href="/blogs"><a className="navbar-link">Blog</a></Link>
+                        <Link href="/speaker"><a className="navbar-link">Nominate a Speaker</a></Link>
+                        <Link href="/team"><a className="rounded-b navbar-link">Meet the Team</a></Link>
                     </ul>
                 </div>
             </nav>
-        </>
+        </div>
     );
+}
+
+Navbar.defaultProps = {
+    hide: 'opacity-100'
 }
 
 export default Navbar;
