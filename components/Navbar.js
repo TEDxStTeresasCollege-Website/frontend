7ed185a784f7471bede2ca1517372b20adf10628
Sharun E Rajeev <sharunpublic@gmail.com>
2021-11-06 08:12:22 +0530

    Navbar mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,17 +5,19 @@ const Navbar = () => {
     return (
         <>
             <nav className="absolute top-0 flex items-center w-full z-10">
-                <img src="/navbar/Header.png" className="absolute -z-10" />
-                <div className="flex-1 px-2 ml-5">
+                <div className="absolute -z-10 w-full h-56">
+                    <Image className="relative object-cover" src="/navbar/navbarbgmobile.svg" layout='fill' />
+                </div>
+                <div className="flex-1 px-2 ml-2 md:ml-5">
                     <Link href="/"><Image src="/navbar/tedXSTClogo.svg" width="250%" height="100%" /></Link>
                 </div>
-                <div className="dropdown mr-5">
+                <div className="dropdown mr-2 md:mr-5">
                     <button className="bg-transparent text-gray-100 font-semibold py-2 px-4 rounded inline-flex items-center">
                         <svg className="w-6 h-6" fill="none" stroke="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="{2}" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </button>
-                    <ul className="dropdown-menu absolute hidden text-gray-100 pt-1 right-5 text-right">
+                    <ul className="dropdown-menu absolute hidden text-gray-100 pt-1 right-1 md:right-5 text-right">
                         <Link href="/"><a className="rounded-t bg-black hover:bg-gray-800 py-4 px-8 block">Home</a></Link>
                         <Link href="/about"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">About TED/TEDx</a></Link>
                         <Link href="/about"><a className="bg-black hover:bg-gray-800 py-4 px-8 block">TEDxStTeresasCollege</a></Link>
