9aee2e1b7d012c0244195294b7cadd0a3b674ced
Sharun E Rajeev <sharunpublic@gmail.com>
2021-11-06 10:30:01 +0530

    Footer fix

--- components/Footer.js
+++ components/Footer.js
@@ -5,18 +5,18 @@ import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-sv
 
 const Footer = () => {
     return (
-        <footer className="text-gray-100 relative bg-black -z-30 min-h-full">
+        <footer className="text-xs md:text-md text-gray-100 relative bg-black -z-30 min-h-full mt-44">
 
-            <div className="absolute -z-20 bottom-0"><img className="bg-cover" src="/footer/footer.png"></img></div>
-            <div className="absolute -z-10 w-1/3 right-14 bottom-14"><img src="/footer/boat.png"></img></div>
-            <div className="absolute -z-10 bottom-0"><img width="40%" src="/footer/net.png"></img></div>
-            <div className="absolute bg-gray-900 text-white rounded text-center w-1/4 p-3 left-1/2 top-10 opacity-0 hover:opacity-100">
+            <div className="absolute -z-20 bottom-0"><img className="bg-cover" src="mobile/footer/footer.png"></img></div>
+            {/* <div className="absolute -z-10 w-1/3 right-14 bottom-14"><img src="/footer/boat.png"></img></div> */}
+            {/* <div className="absolute -z-10 bottom-0"><img width="40%" src="/footer/net.png"></img></div> */}
+            {/* <div className="absolute bg-gray-900 text-white rounded text-center w-1/4 p-3 left-1/2 top-10 opacity-0 hover:opacity-100">
                 <h3 className="text-lg">Kumbalangi</h3>
                 <p className="text-sm">This quaint fishing hamlet is a must-visit for experiencing Kerala's culture and delectable cuisine in its raw form.</p>
                 <p className="text-sm">Sea sparkle (കവര്) along the backwaters abutting the Arabian sea, dotted with Chinese fishing nets paints a charming picture.</p>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-10">
-                <div className="text-center md:text-left">
+            </div> */}
+            <div className="grid grid-cols-2 gap-5 px-5 md:p-10">
+                <div className="text-left">
                     Reach out at :
                     <div className="text-blue-400"><a href="">tedxstteresascollege@gmail.com</a></div>
                     <div className="py-2">
@@ -30,7 +30,7 @@ const Footer = () => {
                         <a href="tel:+918078708025">+918078708025</a><br />
                     </div>
                 </div>
-                <div className="text-center md:text-right">
+                <div className="text-right">
                     <a href="https://goo.gl/maps/oNWWs5diruyXFeVe6">
                         Locate us
                         <span><FontAwesomeIcon icon={faMapMarked} className="text-blue-400 ml-2" fixedWidth /></span>
@@ -39,10 +39,10 @@ const Footer = () => {
                         St. Teresa’s College<br />Park Avenue Road<br />Ernakulam<br />Kerala - 682011<br />India<br />
                     </div>
                 </div>
-                <div className="text-center md:text-left">
+                <div className="text-left">
                     Say Hello!
-                    <div className="flex justify-center md:justify-start gap-3 mt-3 -m-">
-                        <a href="https://instagram.com/tedxst_teresascollege"><span><FontAwesomeIcon icon={faInstagram} className="text-blue-400 ml-2" size="2x" /></span></a>
+                    <div className="flex justify-start gap-3 mt-3">
+                        <a href="https://instagram.com/tedxst_teresascollege"><span><FontAwesomeIcon icon={faInstagram} className="text-blue-400" size="2x" /></span></a>
                         <a href="https://www.facebook.com/Tedxstteresascollege-102464092186247/"><span><FontAwesomeIcon icon={faFacebook} className="text-blue-400 ml-2" size="2x" /></span></a>
                         <a href="https://www.linkedin.com/company/tedxstteresascollege"><span><FontAwesomeIcon icon={faLinkedin} className="text-blue-400 ml-2" size="2x" /></span></a>
                     </div>
@@ -51,7 +51,7 @@ const Footer = () => {
                     <form action="\home" className="w-full max-w-sm md:ml-auto">
                         <div className="flex items-center mb-2">
                             <div className="w-1/2">
-                                <label className="block text-gray-100 font-bold text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name"> Name </label>
+                                <label className="block text-gray-100 text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name"> Name </label>
                             </div>
                             <div className="w-1/2">
                                 <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" />
@@ -59,7 +59,7 @@ const Footer = () => {
                         </div>
                         <div className="flex items-center mb-2">
                             <div className="w-1/2">
-                                <label className="block text-gray-100 font-bold text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password"> Email </label>
+                                <label className="block text-gray-100 text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password"> Email </label>
                             </div>
                             <div className="w-1/2">
                                 <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-email" type="email" />
@@ -67,7 +67,7 @@ const Footer = () => {
                         </div>
                         <div className="flex items-center mb-2">
                             <div className="w-1/2">
-                                <label className="block text-gray-100 font-bold text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password"> Organisation </label>
+                                <label className="block text-gray-100 text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password"> Organisation </label>
                             </div>
                             <div className="w-1/2">
                                 <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-1 px-3 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-organisation" type="text" />
@@ -76,7 +76,7 @@ const Footer = () => {
                         <div className="flex items-center">
                             <div className="w-1/2"></div>
                             <div className="w-1/2">
-                                <input className="ml-auto shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-1 px-3 rounded" type="button" value="Subscribe" />
+                                <input className="ml-auto shadow bg-blue-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white py-1 px-3 rounded" type="button" value="Subscribe" />
                             </div>
                         </div>
                     </form>
