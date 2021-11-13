ab9cb8a835a0e784695509a835c23adb33ae79ad
Sharun E Rajeev <sharunpublic@gmail.com>
2021-11-13 21:10:16 +0530

    Update 1.0

--- pages/test.js
+++ pages/test.js
@@ -1,6 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarked } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 const test = () => {
     return (
         <>
+            
         </>
     );
 }
