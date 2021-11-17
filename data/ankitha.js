733d3b7a971d6f73ebd5fac35de32a89fa70e4e8
sharunrajeev <sharunrajeev25@gmail.com>
2021-11-17 21:39:01 +0530

    Add member list and photos

---
+++ data/ankitha.js
@@ -0,0 +1,16 @@
+var ankitha = [
+    {
+        name: "Marianne Joseph",
+        designation: "Organiser",
+        image: "/member/coreTeam/Neha Neenus.jpg",
+        url: "https://www.instagram.com/marianne_joseph_"
+    },
+    {
+        name: "Marianne Joseph",
+        designation: "Organiser",
+        image: "/member/coreTeam/Neha Neenus.jpg",
+        url: "https://www.instagram.com/marianne_joseph_"
+    }
+]
+
+export default ankitha;
